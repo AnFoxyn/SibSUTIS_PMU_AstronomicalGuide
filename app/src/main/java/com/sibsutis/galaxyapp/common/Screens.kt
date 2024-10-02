b0781ed55c12eaaa52a127122c6bf3afeb15b741
Foxyn <ankorn03@yandex.ru>
2024-10-03 01:42:29 +0700

    Добавил адрес для экрана с Кубом

---
+++ app/src/main/java/com/sibsutis/galaxyapp/common/Screens.kt
@@ -0,0 +1,16 @@
+package com.sibsutis.galaxyapp.common
+
+import kotlinx.serialization.Serializable
+
+
+@Serializable
+object AdvScreen
+
+@Serializable
+object FourScreen
+
+@Serializable
+object AstroScreen
+
+@Serializable
+object CubeScreen
